e64e09ad701867a75759a5a091a24aa25e33426e
Martin Wendt <mar10@wwwendt.de>
2022-07-30 13:16:06 +0200

    Sphinx

---
+++ docs/sphinx/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "yabs_test"
+copyright = "2020, Martin Wendt"
+author = "Martin Wendt"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinxcontrib.mermaid",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    # '.txt': 'markdown',
+    ".md": "markdown",
+}
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
---
+++ docs/sphinx/index.md
@@ -0,0 +1,35 @@
+.. yabs_test documentation master file, created by
+   sphinx-quickstart on Sat Jul 30 12:46:10 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+## Welcome to yabs_test's documentation!
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+```{admonition} NOTE
+:class: warning
+
+Temporary repository and throw-away project to test the
+[yabs](https://github.com/mar10/yabs) tool.
+```
+
+
+## Indices and tables
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
+### Linkify
+
+www.example.com
+
+### Mermaid
+```{mermaid}
+graph LR
+  a --> b
+```
